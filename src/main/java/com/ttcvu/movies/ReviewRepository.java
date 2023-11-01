8483658b723fc5956410938e468b6b05962313b8
Thomas Vu <thomas.vu@hotmail.com>
2023-11-01 21:13:26 +0100

    first commit

---
+++ src/main/java/com/ttcvu/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.ttcvu.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
